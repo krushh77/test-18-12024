906c536c316bbd116068d66ef641c6350d25688e
krushh77 <145192360+krushh77@users.noreply.github.com>
2024-02-18 14:44:30 +0530

    update

---
+++ test/test.js
@@ -0,0 +1,80 @@
+// var array = [1,2,3,4,5]
+
+// function sum(array){
+//     var result = 0
+//   for(var i=0; i<array.length; i++){
+//            
+//   }
+//   return result
+// }
+// 
+
+
+// var array = "121"
+// function palindrome (string){
+//     for(var i=0; i<Math.floor(string.length/2);i++){
+//         if(string[i] != string[string.length -1- i]){
+//             return "it not palindrome"
+
+//         }
+//         return "it is palindrome"
+//     }
+
+// }
+// console.log(palindrome(array))
+
+
+// var array = "abab"
+// function palindrome (string){
+//     for(var i=0; i<Math.floor(string.length/2);i++){
+//         if(string[i] != string[string.length -1- i]){
+//             return "it not palindrome"
+
+//         }
+//         return "it is palindrome"
+//     }
+
+// }
+// console.log(palindrome(array))
+
+
+// q=7
+// function factoril (value){
+//     var sum=1
+//     for(var i=value; i>0; i--){
+
+//                sum*=i
+//     }
+// console.log(sum)
+// }
+// factoril(5)
+
+
+
+
+
+
+
+
+
+
+
+
+// var array = 1
+
+// function usingwhile (num) {
+// var i =0
+//     while(){
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
